d97426123f3d016be1efff4de24574de38fe96c9
rian-ol <64734797+rian-ol@users.noreply.github.com>
2022-04-04 13:47:45 +0100

    Add files via upload

---
+++ zorkproject.cpp
@@ -0,0 +1,5 @@
+#include "zorkproject.h"
+
+ZorkProject::ZorkProject()
+{
+}
---
+++ zorkproject.h
@@ -0,0 +1,12 @@
+#ifndef ZORKPROJECT_H
+#define ZORKPROJECT_H
+
+#include "zorkProject_global.h"
+
+class ZORKPROJECT_EXPORT ZorkProject
+{
+public:
+    ZorkProject();
+};
+
+#endif // ZORKPROJECT_H
