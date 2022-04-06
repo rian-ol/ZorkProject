58e2501fa43519db9168bbdc4bb4f8e9ad754fca
rian-ol <64734797+rian-ol@users.noreply.github.com>
2022-04-06 13:41:13 +0100

    Add files via upload

---
+++ RNG.cpp
@@ -0,0 +1,72 @@
+//random number generator for puzzle room.
+
+#include <iostream>
+#include <ctime>
+#include <string>
+std::string guess;
+std::string answer;
+int guessCount = 0;
+
+bool checkInt(std::string);
+void checkAns(std::string);
+void createAns();
+
+int main(){
+    createAns();
+    
+       while(guessCount!=5 && guess!=answer){
+            std::cout << "Please enter a random number"<<std::endl;
+            std::cin>>guess;
+        
+         if (checkInt(guess))
+            {
+                checkAns(guess);
+            }else{
+            std::cout << "Please enter a valid number"<<std::endl;
+             }
+        }    
+    
+    
+    
+    
+    return 0;
+}
+
+
+bool checkInt(std::string number){
+    std::string actualNum = "";
+    for(int i=0;i<number.length();i++){
+        if (isdigit(number[i]))
+        {
+            actualNum = actualNum + number[i];
+           // std::cout << actualNum<<std::endl;
+        }
+    }
+    if (number==actualNum)
+    {
+        return true;
+    }
+    guess = number;
+    return false;
+}
+
+void checkAns(std::string){
+    guessCount++;
+    if (guess == answer)
+    {
+        std::cout << "Congrats! You guessed correctly."<<std::endl;
+        std::cout << "It took you "<<guessCount<<" guesse(s)."<<std::endl;
+    }else{
+        std::cout << "That was the incorrect guess."<<std::endl;
+        std::cout << "You have guessed "<< guessCount<< " amount of time(s). You have "<< 5-guessCount<<" guesse(s) left."<<std::endl;
+    }
+    
+}
+
+
+void createAns(){
+  srand((unsigned) time(0));
+    int answerInt = ((rand()%10)+1); 
+    answer = std::to_string(answerInt);
+    // std::cout << answer << std::endl;
+}
