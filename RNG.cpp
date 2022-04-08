7f4801bf12518d04ac429f38602c02ee6ff6bd0e
rian-ol <64734797+rian-ol@users.noreply.github.com>
2022-04-08 02:52:46 +0100

    Update RNG.cpp

--- RNG.cpp
+++ RNG.cpp
@@ -2,30 +2,30 @@
 #include "RNG.h"
 
 
- 
+
 
 
 int RNG(){
     guessCount=0;
-    RNGgameCount =1;
+    //RNGgameCount =1;
     RNGclass myRNG;
     myRNG.createAns();
-    
+
        while(guessCount!=5 && guess!=answer){
             std::cout << "Please enter a random number"<<std::endl;
             std::cin>>guess;
-        
+
          if (myRNG.checkInt(guess))
             {
                 myRNG.checkAns(guess);
             }else{
             std::cout << "Please enter a valid number"<<std::endl;
              }
-        }    
-    
-    
-    
-    
+        }
+
+
+
+
     return 0;
 }
 
@@ -57,8 +57,17 @@ void checkAns(std::string){
         std::cout << "That was the incorrect guess."<<std::endl;
         std::cout << "You have guessed "<< guessCount<< " amount of time(s). You have "<< 5-guessCount<<" guesse(s) left."<<std::endl;
     }
-    
+
 }
+
+
+void createAns(){
+  srand((unsigned) time(0));
+    int answerInt = ((rand()%10)+1);
+    answer = std::to_string(answerInt);
+    // std::cout << answer << std::endl;
+}
+
 
 
 void createAns(){
